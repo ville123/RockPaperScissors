73839f6fb7d2f4c0eb20212ed4b70c0dd67ee106
ville123 <t6nivi02@students.oamk.fi>
2019-12-04 15:48:17 +0200

    Added a comment line

--- network.py
+++ network.py
@@ -4,7 +4,7 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "87.92.78.202"
+        self.server = "87.92.78.202" # local network IPv4 address
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p = self.connect()
